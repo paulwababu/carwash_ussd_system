b1fb65778d32f652621fcfecad439804c4e71dfc
michaelmurwayi <mikemurwayi7@gmail.com>
2021-07-02 18:33:00 +0300

    adding flask layout for ussd callback url

---
+++ ussd.py
@@ -0,0 +1,106 @@
+from africastalking.Service import Service
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask_ngrok import run_with_ngrok
+import africastalking
+from werkzeug.wrappers.response import Response 
+
+app = Flask(__name__)
+run_with_ngrok(app)
+username = "sandbox"
+api_key = "17f2d3cb46b40d864a084736b597637efd73f309fed924d1fdb04640bdaed6aa"
+africastalking.initialize(username, api_key)
+sms = africastalking.SMS
+
+@app.route('/api/ussd/callback', methods=['POST'])
+def ussd_callback():
+    session_id = request.values.get("sessionId", None)
+    serviceCode = request.values.get("serviceCode", None)
+    phoneNumber = request.values.get("phonenumber", None)
+    text = request.values.get("text", None)
+    #serve menus based on text
+    #ussd logic
+    if text == "":
+        #Car Wash Main Menu
+        response = "CON Welcome to A+ car wash.Pick The Service you want\n"
+        response += "1. Exclusive Wash \n"
+        response += "2. Full car wash \n"
+        response += "3. Body wash"
+        response += "4. Interior Wash\n"
+        response += "5. Waxing"
+        response += "6. Dusting"
+        response += "7. Carpet Wash"
+    elif text == "1":
+        # sub menu 1
+        #Select Type of Car
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+    
+    elif text == "2":
+        #sub menu 2
+        # Selecting Type of Car
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+    elif text == "3":
+        # sub menu 3
+        # Selecting Type of Car 
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+    elif text == "4":
+        # sub menu 4 
+        # Selecting Type of Car
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+    elif text == "5":
+        # sub menu 5 
+        # Selecting Type of Car
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+    elif text == "6":
+        # sub menu 6 
+        # Selecting Type of Car
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+    elif text == "7":
+        # sub menu 7 
+        # Selecting Type of Car
+        response = "CON Enter Typer of Car\n"
+        response += "1. Sedan"
+        response += "2. Suv"
+        response += "3. Truck (pick-ups)"
+        response += "4. Lorry"
+
+    elif text == "1*1":
+        #ussd menus are split using *
+        pass
+    elif text == "1*2":
+        response = "CON Enter Vehicle Registration "
+    else:
+        response = "END Invalid input. Try again."
+
+    return response        
+     
+
+
+
+if __name__ == "__main__":
+        app.run()
